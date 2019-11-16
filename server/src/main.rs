9c9d7852f13cbbc1242e0466ff35ee5e811640e4
Johannes Maibaum <jmaibaum@gmail.com>
2019-11-16 20:01:44 +0100

    Switch to #[get] attribute instead of .route(...)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use actix_files::Files;
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 
+#[get("/greet/{name}")]
 fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello, {}!\n", &name)
@@ -9,7 +10,7 @@ fn greet(req: HttpRequest) -> impl Responder {
 fn main() {
     HttpServer::new(|| {
         App::new()
-            .route("/greet/{name}", web::get().to(greet))
+            .service(greet)
             // For all other paths, redirect to static file
             .service(Files::new("/", "static/").index_file("index.html"))
     })
